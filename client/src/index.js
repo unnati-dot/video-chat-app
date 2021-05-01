8e1381fcf04c0394df39af0e424ed4232699cab0
unnatiaggarwal <unnatiaggarwal90@gmail.com>
2021-05-02 00:06:12 +0530

    reactjs

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ContextProvider } from './Context';
+
+
+
+ReactDOM.render(
+ <ContextProvider>
+
+<App />
+ </ContextProvider>
+    
+  ,
+  document.getElementById('root')
+);
+
